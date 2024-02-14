4a5a663b2f73bf9942a87f2e6782708fd004e04b
avrsn <mbpurdy@asu.edu>
2024-02-13 23:30:43 -0500

    Add picsumAPI requests

--- photogen/views.py
+++ photogen/views.py
@@ -2,19 +2,59 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import RandomImage
 from django.template import loader
+from .forms import HomeForm
+import requests
+import base64
 
 # Create your views here.
+# nasa api key mbpurdy@asu.edu
+# T3K8debLg8Ux7vKfbn3w00lTUPTkJuuUog7VbCwE 
 def index(request):
-    # title = "Spacepic<br>"
-    # images = RandomImage.objects.order_by("image_num")[:5]
-    # #formatted_images = "<br>".join([str(q.image_num) for q in images])
-    # formatted_images = "<br>".join([f"{str(q.image_num)} - {q.image_url}" for q in images])
-    
-    # output = title + "<br>" + formatted_images
-    # return HttpResponse(output)
-    images = RandomImage.objects.order_by("image_num")[:5]
     template = loader.get_template("photogen/index.html")
-    context = {
-        "images": images,
-        }
-    return HttpResponse(template.render(context, request))
+    form = HomeForm()
+    context = {"form": form}
+    
+
+
+    # api_url = 'https://picsum.photos/200'
+
+    # response = requests.get(api_url)
+    
+    # if response.status_code == 200:
+    #         data = response.content
+    #         return HttpResponse(data, content_type='image/jpeg')
+    # else:
+    #     return HttpResponse("FAILED")
+
+
+
+    
+    if request.method == 'POST':
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            numpics = form.cleaned_data['post']
+            #images = RandomImage.objects.order_by("image_num")[:5]
+
+            api_url = 'https://picsum.photos/200'
+            photoset = {}
+            for i in range(numpics):
+                response = requests.get(api_url)
+                if response.status_code == 200:
+                    images = response.content
+                    base64_image = base64.b64encode(images).decode('utf-8')
+                    photoset[f"key_{i}"] = f"data:image/jpeg;base64,{base64_image}"
+                else:
+                    return HttpResponse("FAILED2")
+
+            
+            context = {
+                "images": photoset,
+                "form": form,
+                "text": numpics
+                }
+
+            return HttpResponse(template.render(context, request))
+        else:
+            return HttpResponse('Form not valid???')
+    else:
+        return HttpResponse(template.render(context, request))   
