4e3b35377c88c35fffcaa4f733304988f4f9c43c
avrsn <mbpurdy@asu.edu>
2024-02-13 23:34:44 -0500

    Add form validation for numbers one through five

--- photogen/forms.py
+++ photogen/forms.py
@@ -3,3 +3,9 @@ from django import forms
 
 class HomeForm(forms.Form):
     post = forms.IntegerField()
+    
+    def clean_post(self):
+        numpics = self.cleaned_data['post']
+        if numpics < 1 or numpics > 5:
+            raise forms.ValidationError('Number of pictures must be 1 - 5')
+        return numpics
