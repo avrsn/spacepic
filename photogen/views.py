df20b7ee803cbcdbb86f162d47cbd7a1ba8b038e
avrsn <mbpurdy@asu.edu>
2024-02-14 00:55:59 -0500

    add zip buffer

--- photogen/views.py
+++ photogen/views.py
@@ -5,49 +5,52 @@ from django.template import loader
 from .forms import HomeForm
 import requests
 import base64
+import zipfile
+import io
+
 
 # Create your views here.
-# nasa api key mbpurdy@asu.edu
-# T3K8debLg8Ux7vKfbn3w00lTUPTkJuuUog7VbCwE 
+
 def index(request):
     template = loader.get_template("photogen/index.html")
     form = HomeForm()
     context = {"form": form}
-    
-
-
-    # api_url = 'https://picsum.photos/200'
-
-    # response = requests.get(api_url)
-    
-    # if response.status_code == 200:
-    #         data = response.content
-    #         return HttpResponse(data, content_type='image/jpeg')
-    # else:
-    #     return HttpResponse("FAILED")
-
-
-
-    
+        
     if request.method == 'POST':
         form = HomeForm(request.POST)
         if form.is_valid():
             numpics = form.cleaned_data['post']
-            #images = RandomImage.objects.order_by("image_num")[:5]
 
             api_url = 'https://picsum.photos/200'
             photoset = {}
+            zippable = []
             for i in range(numpics):
                 response = requests.get(api_url)
                 if response.status_code == 200:
                     images = response.content
+
                     base64_image = base64.b64encode(images).decode('utf-8')
                     photoset[f"key_{i}"] = f"data:image/jpeg;base64,{base64_image}"
+                    
+                    zippable.append(response.content)               
+
                 else:
                     return HttpResponse("FAILED2")
 
+
+            zip_buffer = io.BytesIO()
+            with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+                for i, image in enumerate(zippable):
+                    zip_file.writestr(f'image_{i}.jpg', image)
+                    
+            zip_buffer.seek(0)
+            
+            #response = HttpResponse(zip_buffer, content_type='application/zip')
+            #response['Content-Disposition'] = 'attachment; filename=images.zip'
+           
             
             context = {
+                "zip": zip_buffer,
                 "images": photoset,
                 "form": form,
                 "text": numpics
@@ -59,4 +62,5 @@ def index(request):
             context['form_validation_error_message'] = "Please enter a number between 1 and 5."
             return HttpResponse(template.render(context, request))
     else:
-        return HttpResponse(template.render(context, request))   
+        return HttpResponse(template.render(context, request)) 
+    
